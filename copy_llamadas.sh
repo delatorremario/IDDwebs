c33d46f4d8d4784c2e84233dc946a8e776c8ff55
Mario de la Torre <mariodelatorre@MBP-de-Mario.iptv.microsoft.com>
2017-01-23 15:33:12 -0300

    fix llamadas folder

--- copy_llamadas.sh
+++ copy_llamadas.sh
@@ -1,6 +1,6 @@
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/*.* ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/css ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/fonts ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/img ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/js ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/libs ubuntu@34.196.82.38:/var/www/html/sanpedro 
+scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/*.* ubuntu@34.196.82.38:/var/www/html/llamadas
+scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/css ubuntu@34.196.82.38:/var/www/html/llamadas
+scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/fonts ubuntu@34.196.82.38:/var/www/html/llamadas
+scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/img ubuntu@34.196.82.38:/var/www/html/llamadas
+scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/js ubuntu@34.196.82.38:/var/www/html/llamadas
+scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/libs ubuntu@34.196.82.38:/var/www/html/llamadas 
