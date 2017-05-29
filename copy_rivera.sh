99ea35333ee84e739213ccf914fe88a3b38fc692
Mario de la Torre <delatorremario@gmail.com>
2017-05-29 13:08:07 -0300

    Delete copy_rivera.sh

--- copy_rivera.sh
+++
@@ -1,6 +0,0 @@
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/rivera/*.* ubuntu@34.196.82.38:/var/www/html/rivera
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/rivera/css ubuntu@34.196.82.38:/var/www/html/rivera
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/rivera/fonts ubuntu@34.196.82.38:/var/www/html/rivera
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/rivera/img ubuntu@34.196.82.38:/var/www/html/rivera
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/rivera/js ubuntu@34.196.82.38:/var/www/html/rivera
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/rivera/libs ubuntu@34.196.82.38:/var/www/html/rivera 
