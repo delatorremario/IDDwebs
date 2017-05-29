3ca41d3e6da1507fe9b2e84ef8d5baad5712dad5
Mario de la Torre <delatorremario@gmail.com>
2017-05-29 13:07:34 -0300

    Delete copy_hispanidad.sh

--- copy_hispanidad.sh
+++
@@ -1,6 +0,0 @@
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/hispanidad/*.* ubuntu@34.196.82.38:/var/www/html/hispanidad
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/hispanidad/css ubuntu@34.196.82.38:/var/www/html/hispanidad
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/hispanidad/fonts ubuntu@34.196.82.38:/var/www/html/hispanidad
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/hispanidad/img ubuntu@34.196.82.38:/var/www/html/hispanidad
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/hispanidad/js ubuntu@34.196.82.38:/var/www/html/hispanidad
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/hispanidad/libs ubuntu@34.196.82.38:/var/www/html/hispanidad 
