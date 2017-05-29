af24da9802357c73a2a5ca3733a37282e52fb746
Mario de la Torre <delatorremario@gmail.com>
2017-05-29 13:07:04 -0300

    Delete copy_bioparque.sh

--- copy_bioparque.sh
+++
@@ -1,6 +0,0 @@
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/bioparque/*.* ubuntu@34.196.82.38:/var/www/html/bioparque
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/bioparque/css ubuntu@34.196.82.38:/var/www/html/bioparque
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/bioparque/fonts ubuntu@34.196.82.38:/var/www/html/bioparque
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/bioparque/img ubuntu@34.196.82.38:/var/www/html/bioparque
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/bioparque/js ubuntu@34.196.82.38:/var/www/html/bioparque
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/bioparque/libs ubuntu@34.196.82.38:/var/www/html/bioparque 
