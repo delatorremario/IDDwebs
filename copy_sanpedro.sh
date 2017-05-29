2b204b1a72fb04f677f5f74b34b748cc76988cec
Mario de la Torre <delatorremario@gmail.com>
2017-05-29 13:08:19 -0300

    Delete copy_sanpedro.sh

--- copy_sanpedro.sh
+++
@@ -1,6 +0,0 @@
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/*.* ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/css ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/fonts ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/img ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/js ubuntu@34.196.82.38:/var/www/html/sanpedro
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/sanpedro/libs ubuntu@34.196.82.38:/var/www/html/sanpedro 
