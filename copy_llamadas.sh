1a060ae9a3926c6ca612b2660f2839817e2763ee
Mario de la Torre <delatorremario@gmail.com>
2017-05-29 13:07:45 -0300

    Delete copy_llamadas.sh

--- copy_llamadas.sh
+++
@@ -1,6 +0,0 @@
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/*.* ubuntu@34.196.82.38:/var/www/html/llamadas
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/css ubuntu@34.196.82.38:/var/www/html/llamadas
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/fonts ubuntu@34.196.82.38:/var/www/html/llamadas
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/img ubuntu@34.196.82.38:/var/www/html/llamadas
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/js ubuntu@34.196.82.38:/var/www/html/llamadas
-scp -i ~/.ssh/ubuntu-mario.pem -r /Applications/XAMPP/htdocs/idd/llamadas/libs ubuntu@34.196.82.38:/var/www/html/llamadas 
